2f4b263d7937e0c86d9afb0f2fa9b80e6ab089f1
Rajeev Godambe <rajeev.godambe@livehealth.in>
2022-06-21 16:53:00 +0530

    Added Authentication model for template

---
+++ user_auth/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email,and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(email, password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(verbose_name='email address', unique=True,)
+    full_name = models.CharField(max_length=200)
+    contact = models.CharField(max_length=20, blank=True, null=True)
+    country_code = models.CharField(max_length=10, default='91')
+    country = models.CharField(max_length=100,default='India')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    hash = models.CharField(max_length=200,blank=True, null=True)
+    avatar = models.CharField(default='', max_length=300)
+    date_joined = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
